37feb36f4b518d6dc6e5da717d0464ce3cfbe5a0
Scott Butler <scottbutler2005@gmail.com>
2019-09-07 18:05:42 +1000

    Fixing a bug where the misplaced count was being incorrectly calculated. Closes #1

--- Mastermind.ConsoleApp/Mastermind.cs
+++ Mastermind.ConsoleApp/Mastermind.cs
@@ -1,27 +1,58 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Mastermind
 {
     public static class Core
     {
         public static GuessResult Evaluate(Colours[] secret, Colours[] guess)
-        {                
+        {
+            if (secret == guess)
+            {
+                return new GuessResult(wellPlaced: secret.Length, misplaced: 0);
+            }
+
             var wellPlaced = 0;
-            var misplaced = 0;
+            var misplacedPerColour = BuildColourDictionary((x) => 0);
+            var secretCountPerColour = BuildColourDictionary((x) => secret.Count(s => s == x));
+
             for (var i = 0; i < guess.Length; i++)
             {
-                if (Array.Exists(secret, x => x == guess[i])
-                    && secret[i] != guess[i])
-                    {
-                        misplaced++;
-                    }
-                
                 if (secret[i] == guess[i])
                 {
                     wellPlaced++;
                 }
+                else
+                {
+                    if (Array.Exists(secret, x => x == guess[i])
+                        && secret[i] != guess[i]
+                        && misplacedPerColour[guess[i]] < secretCountPerColour[guess[i]]
+                        )
+                    {
+                        misplacedPerColour[guess[i]]++;
+                    }
+                }
             }
-            return new GuessResult(wellPlaced: wellPlaced, misplaced: misplaced);
+
+            return new GuessResult(
+                wellPlaced: wellPlaced, 
+                misplaced: misplacedPerColour.Select(x => x.Value).Sum()
+            );
+        }
+
+        private static Colours Parse(string input)
+        {
+            return (Colours)Enum.Parse(typeof(Colours), input);
+        }
+
+        private static Dictionary<Colours, int> BuildColourDictionary(Func<Colours, int> func)
+        {
+            var colourNames = Enum.GetNames(typeof(Colours));
+
+            return colourNames
+                .Select(x => Parse(x))
+                .ToDictionary<Colours, Colours, int>(a => a, (a) => func(a));
         }
     }
 
